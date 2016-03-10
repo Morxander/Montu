e8ff3cc5aeacd8fd4446dc7d0e847a7e033e2693
Morad Edwar <me@morad-edwar.com>
2016-03-11 01:27:11 +0200

    Make the installation and Utilities files

---
+++ install.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sqlite3
+import os
+from utilities import bcolors
+from utilities import hashing
+from utilities import savingToDb
+
+# Printing Logo
+print bcolors.HEADER + """
+-----------------------------------
+|  ___  ___            _          |
+|  |  \/  |           | |         |
+|  | .  . | ___  _ __ | |_ _   _  |
+|  | |\/| |/ _ \| '_ \| __| | | | |
+|  | |  | | (_) | | | | |_| |_| | |
+|  \_|  |_/\___/|_| |_|\__|\__,_| |
+|                                 |
+|         Version 0.1             |
+|       By : Morad Edwar          |
+-----------------------------------
+"""
+# Opening the database file for writing
+conn = sqlite3.connect("files.db")
+print bcolors.OKGREEN + "Opened database successfully"
+# Creating the settings table
+conn.execute('''CREATE TABLE SETTINGS
+       (OPTION_KEY           TEXT    NOT NULL,
+        OPTION_VALUE         TEXT     NOT NULL);''')
+print bcolors.OKGREEN + "Created settings table successfully"
+# Creating the files table
+conn.execute('''CREATE TABLE FILES
+       (FILE_PATH           TEXT    NOT NULL,
+       FILE_HASH           TEXT     NOT NULL,
+       UPDATED_AT          TEXT);''')
+print bcolors.OKGREEN + "Created files table successfully"
+# Let's start the magic
+full_path = raw_input(bcolors.ENDC + "Please enter the full path to monitor : ")
+while not os.path.exists(full_path) or not os.path.isdir(full_path):
+  full_path = raw_input(bcolors.FAIL + "Please enter valid full path to monitor : ")
+# Saving the path to the settings table
+conn.execute("INSERT INTO settings (OPTION_KEY,OPTION_VALUE) VALUES('full_path', '"+full_path+"');");
+conn.commit()
+print bcolors.OKGREEN + "Path saved successfully"
+# Saving the files list
+savingToDb.getFilesList(full_path, conn)
+
+conn.commit()
+conn.close()
---
+++ utilities.py
@@ -0,0 +1,37 @@
+import os
+
+class bcolors:
+  HEADER = '\033[95m'
+  OKBLUE = '\033[94m'
+  OKGREEN = '\033[92m'
+  WARNING = '\033[93m'
+  FAIL = '\033[91m'
+  ENDC = '\033[0m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+
+class hashing:
+  @staticmethod
+  def hashfile(file_path):
+    hash = os.popen("md5sum " + file_path + " | cut -d' ' -f1").read().replace('\n','')
+    return hash
+
+class savingToDb:
+  @staticmethod
+  # Method to save file to DB
+  def saveFileToDB(conn, full_path, file_name):
+    conn.execute("INSERT INTO FILES (FILE_PATH,FILE_HASH) \
+        VALUES('" + full_path + file_name + "', '" + hashing.hashfile(full_path + file_name) + "');");
+  @staticmethod
+  # Method to get the dir files
+  def getFilesList(full_path,conn):
+    list_of_files = os.listdir(full_path)
+    list_of_dirs = next(os.walk(full_path))[1]
+    for file_name in list_of_files:
+      if full_path[-1] != "/":
+        full_path = full_path + "/"
+      if os.path.isfile(full_path + file_name):
+        savingToDb.saveFileToDB(conn, full_path, file_name)
+    for dir_name in list_of_dirs:
+      if os.path.isdir(dir_name):
+        savingToDb.getFilesList(full_path + dir_name,conn)
